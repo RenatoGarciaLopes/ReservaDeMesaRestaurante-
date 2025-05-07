1e137134a53ca53359b609a8fcfa00520e7e6391
RenatoGarciaLopes <renatovideos41@gmail.com>
2025-05-07 20:25:41 -0300

    ItemDeCardapio repository and entities

---
+++ demo/src/main/java/com/example/demo/entities/ItemDeCardapio.java
@@ -0,0 +1,43 @@
+package com.example.demo.entities;
+
+import java.math.BigDecimal;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.persistence.GenerationType;
+
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
+@Entity
+public class ItemDeCardapio {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    
+
+
+}
