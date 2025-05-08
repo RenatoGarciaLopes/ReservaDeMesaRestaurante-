87668dc6e0a1fb6d7a8095f7989911d38c1e165d
Olival42 <phsolival@gmail.com>
2025-05-08 07:51:09 -0300

    Entidade e repositorio da mesa criados

---
+++ demo/src/main/java/com/example/demo/entities/Mesa.java
@@ -0,0 +1,41 @@
+package com.example.demo.entities;
+
+import com.example.demo.enums.StatusMesa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "mesas")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Mesa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Integer numero;
+
+    @Column(nullable = false)
+    private Integer capacidade;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusMesa status;
+}
