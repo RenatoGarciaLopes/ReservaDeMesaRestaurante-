d751a36bccac692ffa1574781ff29ab99cff811a
RenatoGarciaLopes <renatovideos41@gmail.com>
2025-05-08 22:20:48 -0300

    foi

--- demo/src/main/java/com/example/demo/mapper/ItensMapper.java
+++ demo/src/main/java/com/example/demo/mapper/ItensMapper.java
@@ -3,6 +3,7 @@ package com.example.demo.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.example.demo.dto.CadastrarItensDto;
 import com.example.demo.dto.ListarItensDto;
@@ -11,6 +12,7 @@ import com.example.demo.entities.ItemDeCardapio;
 @Mapper(componentModel = "spring")
 public interface ItensMapper {
 
+    @Mapping(target = "id", ignore = true)
     ItemDeCardapio toEntity(CadastrarItensDto itensDto);
     ListarItensDto tDto(ItemDeCardapio itemDeCardapio);
     List<ListarItensDto> toDtoList(List<ItemDeCardapio> itemsDeCardapio);
