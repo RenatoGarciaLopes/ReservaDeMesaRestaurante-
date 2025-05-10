5d3ec30fdeb037c3dde9e3afe68b750959b313e9
Olival42 <phsolival@gmail.com>
2025-05-10 14:49:24 -0300

    Criei a entidade e o repositorio pedido

---
+++ demo/src/main/java/com/example/demo/entities/Pedido.java
@@ -0,0 +1,44 @@
+package com.example.demo.entities;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pedidos")
+@Data
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = true)
+    @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_item")
+    private Reserva reserva;
+
+    @OneToMany(mappedBy = "pedidos", cascade = CascadeType.PERSIST)
+    private List<PedidoItem> pedidoItens;
+
+    @Column(nullable = false)
+    private BigDecimal valorTotal;
+}
---
+++ demo/src/main/java/com/example/demo/entities/PedidoItem.java
@@ -0,0 +1,34 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class PedidoItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pedido")
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "id_item")
+    private ItemDeCardapio item;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+}
