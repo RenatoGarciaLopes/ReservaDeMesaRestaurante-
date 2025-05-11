5d3b7767fceb610e05a757b3685f6810a64063a9
RenatoGarciaLopes <renatovideos41@gmail.com>
2025-05-11 12:13:56 -0300

    ok

--- demo/target/generated-sources/annotations/com/example/demo/mapper/MesaMapperImpl.java
+++ demo/target/generated-sources/annotations/com/example/demo/mapper/MesaMapperImpl.java
@@ -10,7 +10,11 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
+<<<<<<< HEAD
     date = "2025-05-10T20:03:25-0300",
+=======
+    date = "2025-05-11T12:10:28-0300",
+>>>>>>> bb5baaf (ok)
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.z20250331-1358, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
